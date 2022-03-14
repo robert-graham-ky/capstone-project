56462b294e19925f7857dfa48bcb7b8451824d06
RobDawg-sama <pickles40324@gmail.com>
2022-03-14 03:49:46 -0400

    Using a console.log statement for a bug fix. Pushing to heroku because my api will block my local machine

--- src/components/menu/menu-sidebar-list.js
+++ src/components/menu/menu-sidebar-list.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+console.log(props.menu);
 const MenuSidebarList = props => {
   const menuList = props.menu.map(menuItem => {
     return (
-      <div key={menuItem.id} className="menu-item-thumb">
+      <div key={menuItem.id} className="menu-item-deleter">
         <div className="menu-image-wrapper">
           <img src={menuItem.image} />
         </div>
         <div className="sidebar-text-wrapper">
             <h1 className="sidebar-title">{menuItem.name}</h1>
             <div className="icon-wrapper">
-                <a className="trash-icon" onClick={() => props.handleDeleteClick(MenuItem)}>
+                <a className="trash-icon" onClick={() => props.handleDeleteClick(menuItem)}>
                     <FontAwesomeIcon icon="trash"></FontAwesomeIcon>
                 </a>
             </div>
