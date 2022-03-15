1318be1e87a44ef28587ef2e4a4e47e166ffb893
RobDawg-sama <pickles40324@gmail.com>
2022-03-15 06:17:02 -0400

    added an icons.js file that I copied from my portfolio, then added a constructor function to app.js in order to run a function to add icons to the library

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import Home from "./pages/home";
 import Edit from "./pages/edit";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
 import NavigationContainer from "./navigation/navigation-container";
+import Icons from "../helpers/icons";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    Icons();
+  }
   render() {
     return (
       <div className='app'>
