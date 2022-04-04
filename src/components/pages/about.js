d312db93d7283e7df8252eafcea537b26dd2f2fd
RobDawg-sama <pickles40324@gmail.com>
2022-04-04 02:19:21 -0400

    fixing minor bug in the iframe

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -62,7 +62,7 @@ export default class About extends Component {
         <div className="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3136.001791844937!2d-84.87730398472405!3d38.18663247968658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884274bf073ae28d%3A0x104eaffebb58cd18!2s700%20Capital%20Ave%2C%20Frankfort%2C%20KY%2040601!5e0!3m2!1sen!2sus!4v1649049782128!5m2!1sen!2sus"
-            sandbox="allow-scripts allow-modal"
+            sandbox="allow-scripts"
           ></iframe>
            
         </div>
